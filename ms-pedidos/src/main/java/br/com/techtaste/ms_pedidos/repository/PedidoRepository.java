088a5cbf69fbcf977daa365859200dd769f56b1e
2° Ten Pablo <guimaraes.pablo@CDS.LOCAL>
2025-04-14 15:56:55 -0300

    Subindo os projetos

---
+++ ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/repository/PedidoRepository.java
@@ -0,0 +1,8 @@
+package br.com.techtaste.ms_pedidos.repository;
+
+import br.com.techtaste.ms_pedidos.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
+}
